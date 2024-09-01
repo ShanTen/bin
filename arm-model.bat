1490e71473333095094ba8b8a20b6a44efd89668
Shan10Code <jojogithub@gmail.com>
2024-09-02 01:12:18 +0530

    tells me what stm32 model I have bc I keep forgetting

---
+++ arm-model.bat
@@ -0,0 +1,7 @@
+@echo off 
+rem why the fuck was this so fucking hard to do Im going to fucking kill myself 
+rem every bad fucking line of code that I write in msbatch (all of them) will contribute to the end of me
+rem fuck you ms batch you fucking piece of shit I fucking hate you bastard go fucking die fucking fuck
+rem fuck you 
+
+clip < c:/jojo/jojoBin/tmp_wtf_arm_model.txt
