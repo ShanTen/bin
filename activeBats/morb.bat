5af3a0b10606a5c7130c0cfb3acb1497f86959bb
Shan10Code <jojogithub@gmail.com>
2024-09-10 00:43:35 +0530

    modified morb

--- activeBats/morb.bat
+++ activeBats/morb.bat
@@ -8,5 +8,5 @@ popd
 pushd D:\shan10\myCode\python\logger-new\metaScripts\ 
 C:\Users\jojoh\.pyenv\pyenv-win\versions\3.11.0\python.exe conv2html.py
 popd 
-cdd D:\shan10\myCode\python\logger-new\
+D: && cd D:\shan10\myCode\python\logger-new\
 echo Run batch File to Confirm Post 
