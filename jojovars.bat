97afc7f77b6d3b7dea9dfad03132d8eecd653e0a
Shan10Code <jojogithub@gmail.com>
2024-10-11 23:46:02 +0530

    added file to display/track list of my custom environment variables

---
+++ jojovars.bat
@@ -0,0 +1,7 @@
+@echo off
+echo jojo_bin_home - [%jojo_bin_home%]
+echo jojo_home - [%jojo_home%]
+echo JOJO_NTFY_ID - [%JOJO_NTFY_ID%]
+echo ACTIVE_BATS - [%ACTIVE_BATS%]
+echo.
+echo usage %ACTIVE_BATS%\npp.bat confuse_file
